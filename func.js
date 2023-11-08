1419ce270bfc6750b63e8a69d5818abb4ad1efae
StudentAM <studentam@space.metroed.local>
2023-11-08 09:35:04 -0800

    finale

--- func.js
+++ func.js
@@ -9,6 +9,10 @@ var products = {
         name: "JavaScript Textbook",
         price: 34.95
     },
+    "551943":{
+        name: "Emily",
+        price: 0.01
+    },
     "4549292070248": {
         name: "Xerox Paper",
         price: 10.99
@@ -96,8 +100,41 @@ function addItem(){
         cart.appendChild(quantity);
         cart.classList.add("cart");
         page.appendChild(cart);
+        reciept = document.createElement("div");
+        page.appendChild(reciept);
     }
-    
+    obj = getProduct(barcode.value);
+    total += obj.price * quan.value;
+    thing = document.createElement("div");
+    what = document.createElement("p");
+    howMuch = document.createElement("p");
+    quant = document.createElement("p");
+    what.innerText =obj.name;
+    howMuch.innerText = obj.price;
+    quant.innerText = quan.value;
+    thing.appendChild(what);
+    thing.appendChild(howMuch);
+    thing.appendChild(quant);
+    thing.classList.add("item");
+    reciept.appendChild(thing);
+    if(add === 0){
+        tot = document.createElement("p");
+        tot.innerText = "Total: $" + total;
+        page.appendChild(tot);
+        checkout = document.createElement("button");
+        checkout.innerText = "Checkout";
+        page.appendChild(checkout);
+        checkout.addEventListener("click", checkOut);
+    }
+    tot.innerText = "Total: $" + total;
     add++;
+    quan.value = "";
+    barcode.value = "";
 }
 sub.addEventListener("click", addItem);
+
+function checkOut(){
+    finalTotal = document.createElement("p");
+    finalTotal.innerText = "Your grand total (including tax, 9.25%) is $" + parseFloat(total * 1.0925).toFixed(2);
+    page.appendChild(finalTotal);
+}
